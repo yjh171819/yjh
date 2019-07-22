6e4d848dfa5fec9e70b94a1c97fbbf01b8f8acc0
yjh171819 <1483667602@qq.com>
2019-07-22 10:37:17 +0800

    t

--- 0718/07zuoye.js
+++ 0718/07zuoye.js
@@ -1,31 +1,20 @@
 var http=require("http")
 var fs=require("fs")
 http.createServer((req,res)=>{
-if(req.url=="/facicon.ico"){
+if(req.url=="/favicon.ico"){
     return 
 }
 fs.readdir('./aa',(err,data)=>{
     var arr=[];
-    for(let a=0;a< data.length;a++){
+    for(let a=0;a<data.length;a++){
         fs.stat("./aa/"+data[a],(err,stats)=>{
-            var pd=stats.isDirectory()
-            if(pd){
+            // var pd=stats.isDirectory()
+            if(stats.isDirectory()){
                 arr.push(data[a])
             }
+            console.log(arr)
         })
     }
-    res.end('success')
+    res.end("success")
 })
-// fs.readdir('aa',function(err,files){
-//     var arr=[];
-//     for(var i=0;i<files.length;i++){
-//         var filename=files[i];
-//         var stats=fs.statSync('./aa/'+filename)
-//         if(stats.isDirectory()){
-//             wenjianjia.push(filename); 
-//         }
-//     }
-//     console.log(wenjianjia)
-// })
-// res.end("success")
 }).listen(3000)
